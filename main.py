9e6fb0c5538bb9c9575e6a3863bb8c6ef2b18640
ТараСкрипник <garoldskrypnyk@gmail.com>
2024-01-20 15:47:59 +0200

    Ex2 was done

--- main.py
+++ main.py
@@ -1,4 +1,11 @@
 try:
+    text = str(input('Vvedit text: '))
+    filter_text = text.lower()
+    slova = str(input("Vvedit zarezervovani slova: ")).lower()
+    if slova in filter_text:
+        print('Vashe slovo znaydene: ' + text.replace(slova, slova.upper()))
+    else:
+        print('Vashe slovo ne znaydene')
 
 except Exception as e:
     print(e)
