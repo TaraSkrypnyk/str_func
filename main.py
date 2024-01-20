b7d56a9c007e85ee4b6c9345b9a0805063a719b1
ТараСкрипник <garoldskrypnyk@gmail.com>
2024-01-20 16:03:27 +0200

    Ex3 was done

--- main.py
+++ main.py
@@ -1,4 +1,9 @@
 try:
-
+    text = str(input('Vvedit text: '))
+    sign_for_search = str('.')
+    if sign_for_search in text:
+        print('V vashomu texti ' , text.count(sign_for_search) , 'rechen')
+    else:
+        print('V vashomu texti 1 rechennya')
 except Exception as e:
     print(e)
