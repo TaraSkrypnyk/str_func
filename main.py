b993e0c1ba3ab326028d8b8309b435c0c919ca2a
ТараСкрипник <garoldskrypnyk@gmail.com>
2024-01-20 14:40:39 +0200

    Ex1 was done

--- main.py
+++ main.py
@@ -1,4 +1,9 @@
 try:
-
+    sl1 = str(input('Vvedit slovo: ')).lower()
+    sl2 = sl1[::-1]
+    if sl2 == sl1:
+        print('Vashe slovo palindrom')
+    else:
+        print('Vashe slovo ne palindrom')
 except Exception as e:
     print(e)
