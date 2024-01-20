c82bb3df644a77408dfdac94a13c3e1fb3efa7f1
ТараСкрипник <garoldskrypnyk@gmail.com>
2024-01-20 16:27:01 +0200

    Ex4 was done

--- main.py
+++ main.py
@@ -1,4 +1,9 @@
 try:
-
+    def my_cytata():
+        text = input('Vvedit text: ')
+        author = input('Vvedit authora: ')
+        print("`", text , "`")
+        print(author)
+    my_cytata()
 except Exception as e:
     print(e)
