061c1637822705100d388c1e061bfbfed99422a6
ТараСкрипник <garoldskrypnyk@gmail.com>
2024-01-20 16:58:41 +0200

    Ex5 was done

--- main.py
+++ main.py
@@ -1,4 +1,37 @@
 try:
+    def neparny_chysla():
+        num1 = n1 = int(input('Vvedit pershe chyslo: '))
+        if num1 < 0:
+            num1 = n1 = int(input('Vvedit dodatne chyslo: '))
+        num2 = n2 = int(input('Vvedit druhe chyslo: '))
+        if num2 < 0:
+            num2 = n2 =int(input('Vvedit dodatne chyslo: '))
+        if num2 > num1:
+            if (n1 % 2) == 0:
+                n1 += 1
+                while n1 < n2:
+                    print(n1," ", end="")
+                    n1 += 2
+            elif (n1 % 2) == 1:
+                n1 += 2
+                while n1 < n2:
+                    print(n1," ", end="")
+                    n1 += 2
+        elif num1 > num2:
+            n1 = num2
+            n2 = num1
+            if (n1 % 2) == 0:
+                n1 += 1
+                while n1 < n2:
+                    print(n1," ", end="")
+                    n1 += 2
+            elif (n1 % 2) == 1:
+                n1 += 2
+                while n1 < n2:
+                    print(n1," ", end="")
+                    n1 += 2
+
+    neparny_chysla()
 
 except Exception as e:
     print(e)
