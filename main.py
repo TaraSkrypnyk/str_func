7437e102ce3cdbb57b7774c18a3fe57c5b02eaac
ТараСкрипник <garoldskrypnyk@gmail.com>
2024-01-21 14:09:03 +0200

    Ex8 was done

--- main.py
+++ main.py
@@ -1,4 +1,18 @@
 try:
-
+    def sum():
+        num1 = int(input('Vvedit pershe chyslo: '))
+        num2 = int(input('Vvedit druhe chyslo: '))
+        rez = 0
+        if num2 > num1:
+            while num1<num2-1:
+                num1 += 1
+                rez = rez + num1
+            print(rez)
+        elif num1 > num2:
+            while num2 < num1-1:
+                num2 += 1
+                rez = rez + num2
+            print(rez)
+    sum()
 except Exception as e:
     print(e)
