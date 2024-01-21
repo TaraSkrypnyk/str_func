94c8a52aecb4921e12f895f3753d6c8a2a9759ab
ТараСкрипник <garoldskrypnyk@gmail.com>
2024-01-21 14:34:59 +0200

    Ex9 was done

--- main.py
+++ main.py
@@ -1,4 +1,13 @@
 try:
+    def proste():
+        n = int(input('Vvedit chyslo: '))
+        rez = False
+        if n<0:
+            n = int(input('Vvedit dodatne chyslo: '))
+        if (n%2 != 0) and (n%3 != 0) and (n%5 != 0) and (n%7 != 0) and (n != 1) or (n == 2):
+            rez = True
+        print(rez)
+    proste()
 
 except Exception as e:
     print(e)
