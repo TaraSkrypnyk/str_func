c64f6359533434f685d4ea5bba38fd2e748330c8
ТараСкрипник <garoldskrypnyk@gmail.com>
2024-01-24 19:02:06 +0200

    Ex13 was done

--- main.py
+++ main.py
@@ -1,4 +1,12 @@
 try:
+    num_list = list(map(int, input("Введіть список чисел через пробіл").split()))
+    def listsum(numList):
+        Sum = 0
+        for i in numList:
+            Sum = Sum + i
+        return Sum
+    print("Сума всіх елементів списку: ", listsum(num_list))
+    print("Середнє арефметичне всіх елементів списку: ", listsum(num_list)/len(num_list))
 
 except Exception as e:
     print(e)
