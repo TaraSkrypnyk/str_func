a6da584fde8ae5b74eb92487b8e52a5da8ff5c31
ТараСкрипник <garoldskrypnyk@gmail.com>
2024-01-24 18:50:04 +0200

    Ex12 was done

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
 try:
-
+    num_list= list(map(int, input("Введіть список чисел через пробіл").split()))
+    x = int(input("Введіть число для пошуку в списку"))
+    print(num_list.count(x))
 except Exception as e:
     print(e)
